c8b0f53a9e7c434daf334c554f2371997e1098f5
novalfaiq <novalfaiq12@gmail.com>
2023-06-13 10:56:34 +0800

    add create post

--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -4,12 +4,23 @@
         <h1>{{ thread.title }}</h1>
 
         <PostList :posts="threadPosts"/>
+
+        <div class="col-full">
+            <form @submit.prevent="addNewPost">
+                <div class="form-grup">
+                    <textarea v-model="newPostText" name="" id="" cols="30" rows="10" class="form-input"></textarea>
+                </div>
+                <div class="form-actions">
+                    <button class="btn-blue">Submit post</button>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
 
 <script setup>
 import sourceData from '@/data.json'
-import { computed } from 'vue';
+import { computed, ref } from 'vue'
 import PostList from '@/components/PostList.vue'
 
 // Props
@@ -22,7 +33,8 @@ const props = defineProps({
 
 // Data
 const threads = sourceData.threads
-const posts = sourceData.posts
+const posts = ref(sourceData.posts)
+const newPostText = ref('')
 
 // Computed property
 const thread = computed(() => {
@@ -30,8 +42,24 @@ const thread = computed(() => {
 })
 
 const threadPosts = computed(() => {
-    return posts.filter(post => post.threadId === props.id)
+    return posts.value.filter(post => post.threadId === props.id)
 })
+
+// Methods
+const addNewPost = () => {
+    const postId = 'ggwp' + Math.random()
+    const post = {
+        id: postId,
+        text: newPostText.value,
+        publishedAt: Math.floor(Date.now() / 1000),
+        threadId: props.id,
+        userId: 'VXjpr2WHa8Ux4Bnggym8QFLdv5C3',
+    }
+
+    posts.value.push(post)
+    thread.value.posts.push(postId)
+}
+
 </script>
 
 
